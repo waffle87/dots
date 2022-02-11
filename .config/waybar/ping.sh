be95d40eb6f08284708d57820841824d8351f0c8
jack <applej955@gmail.com>
2022-02-11 00:06:34 -0700

    font version bump

--- .config/waybar/ping.sh
+++ .config/waybar/ping.sh
@@ -2,7 +2,7 @@
 HOST=8.8.8.8
 
 if ! ping=$(ping -n -c 1 -W 1 $HOST); then
-  echo "xxx"
+  echo "XXX"
 else
   rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
   echo "$rtt"
