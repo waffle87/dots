bb1bcb9293f6141aaeb9a6de39b32cc19aa67dbf
jack <0x6A73@pm.me>
2022-03-15 00:49:49 -0600

    remove stuff from readme

--- .config/waybar/output.sh
+++ .config/waybar/output.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #default=$(pactl get-default-sink)
 if [[ $(pactl get-default-sink) = "alsa_output.usb-Schiit_Audio_Schiit_Modi_3_-00.analog-stereo" ]]; then
-  pactl set-default-sink 45
+  pactl set-default-sink 47
 else
-  pactl set-default-sink 43
+  pactl set-default-sink 373
 fi
