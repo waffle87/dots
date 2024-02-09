ccf9ea7b96125104d843c82a9e45bc1cda97ba19
Jack Sangdahl <0x6A73@pm.me>
2024-02-08 22:12:50 -0700

    waybar network module & swaynag

--- .config/scripts/network.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget -q --spider http://google.com
-if [ $? -eq 0 ]; then
-  ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-else
-  echo "disconnected"
-fi
